085691262e5cc50d74d48e39e0ec1446b2fea820
Fernando Medina Corey <fmcorey@gmail.com>
2020-07-28 17:40:05 -0700

    add files working on raspi

---
+++ raspi.py
@@ -0,0 +1,41 @@
+# python3 shadow.py --endpoint REPLACE_ME-ats.iot.us-east-1.amazonaws.com \
+# --cert REPLACE_ME-certificate.pem.crt \
+# --key REPLACE_ME-private.pem.key \
+# --root-ca root-CA.crt \
+# --thing-name raspi \
+# --shadow-property light
+
+from awscrt import auth, io, mqtt, http
+from awsiot import iotshadow
+from awsiot import mqtt_connection_builder
+from concurrent.futures import Future
+import sys
+import threading
+import traceback
+from uuid import uuid4
+
+
+
+import RPi.GPIO as GPIO
+import time
+ledPin = 11
+
+def setup():
+    GPIO.setmode(GPIO.BOARD) # use Physical GPIO Numbering,
+    GPIO.setup(ledPin, GPIO.OUT) # set the ledPin to OUTPUT mode
+    GPIO.output(ledPin, GPIO.LOW) # make ledPin output LOW level
+    print ('using pin%d'%ledPin)
+
+
+def turn_on():
+    GPIO.output(ledPin, GPIO.HIGH)
+    print ('led turned on >>>')
+
+
+def turn_off():
+    GPIO.output(ledPin, GPIO.LOW)
+    print ('led turned off >>>')
+
+
+def destroy():
+    GPIO.cleanup()
